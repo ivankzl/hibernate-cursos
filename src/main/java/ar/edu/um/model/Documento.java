cbfadb858f2672fc125f9bcb3c3eea9baf44a8cc
Laura <lauragadea@gmail.com>
2015-05-14 19:05:05 -0300

    ivaneta vertiginosa

---
+++ src/main/java/ar/edu/um/model/Documento.java
@@ -0,0 +1,12 @@
+package ar.edu.um.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "documento")
+public class Documento {
+
+}
