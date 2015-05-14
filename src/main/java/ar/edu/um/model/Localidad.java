d1f609c3a9b34a937d98a479b79114571f48eaf1
ivankuzel@gmail.com <ivankuzel@gmail.com>
2015-05-14 19:57:06 -0300

    first commit

--- src/main/java/ar/edu/um/model/Localidad.java
+++ src/main/java/ar/edu/um/model/Localidad.java
@@ -22,5 +22,51 @@ public class Localidad {
 	
 	@Column(name = "id", nullable = false)
 	private int id;
+
+	public Localidad() {
+		
+	}
+
+	public int getLoc_Fac_ID() {
+		return Loc_Fac_ID;
+	}
+
+	public void setLoc_Fac_ID(int loc_Fac_ID) {
+		Loc_Fac_ID = loc_Fac_ID;
+	}
+
+	public int getLoc_Prv_ID() {
+		return Loc_Prv_ID;
+	}
+
+	public void setLoc_Prv_ID(int loc_Prv_ID) {
+		Loc_Prv_ID = loc_Prv_ID;
+	}
+
+	public int getLoc_ID() {
+		return Loc_ID;
+	}
+
+	public void setLoc_ID(int loc_ID) {
+		Loc_ID = loc_ID;
+	}
+
+	public String getLoc_Nombre() {
+		return Loc_Nombre;
+	}
+
+	public void setLoc_Nombre(String loc_Nombre) {
+		Loc_Nombre = loc_Nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 	
 }
--- src/main/java/ar/edu/um/model/Persona.java
+++ src/main/java/ar/edu/um/model/Persona.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Id;
 
 public class Persona {
-	
+
 	@Id
 	@Column(name = "Per_ID", nullable = false)
 	private int Per_ID;
@@ -19,5 +19,46 @@ public class Persona {
 	private String Per_Sexo;
 	@Column(name = "clave", nullable = false)
 	private String clave;
+	public Persona() {
+		
+	}
+	public int getPer_ID() {
+		return Per_ID;
+	}
+	public void setPer_ID(int per_ID) {
+		Per_ID = per_ID;
+	}
+	public int getPer_Doc_ID() {
+		return Per_Doc_ID;
+	}
+	public void setPer_Doc_ID(int per_Doc_ID) {
+		Per_Doc_ID = per_Doc_ID;
+	}
+	public String getPer_Apellido() {
+		return Per_Apellido;
+	}
+	public void setPer_Apellido(String per_Apellido) {
+		Per_Apellido = per_Apellido;
+	}
+	public String getPer_Nombre() {
+		return Per_Nombre;
+	}
+	public void setPer_Nombre(String per_Nombre) {
+		Per_Nombre = per_Nombre;
+	}
+	public String getPer_Sexo() {
+		return Per_Sexo;
+	}
+	public void setPer_Sexo(String per_Sexo) {
+		Per_Sexo = per_Sexo;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	
 
 }
